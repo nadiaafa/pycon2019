1fd7340be5e04a5c01a81be00dd3321a85578713
Nadia AFAKROUCH <nafakrouch@bloopark.de>
2019-10-29 14:29:48 +0100

    [ADD] computed field

--- extra-addons/pycon/models/rooms.py
+++ extra-addons/pycon/models/rooms.py
@@ -6,3 +6,9 @@ class PyconRoom(models.Model):
 	name = fields.Char(string="Name", required=True)
 	capacity = fields.Integer()
 	presentations = fields.One2many('pycon.presentation', 'room', 'Presentations')
+	nbr_presentations = fields.Integer(compute="_compute_nbr_presentations")
+
+	@api.depends('presentations')
+	def _compute_nbr_presentations(self):
+	    for record in self:
+	        record.nbr_presentations = len(record.presentations)
