a57cb6a1d41dfbb9a08b4491245fefc850b6861a
Nadia AFAKROUCH <nafakrouch@bloopark.de>
2019-10-29 14:51:09 +0100

    [ADD] Access rights

--- extra-addons/pycon/__manifest__.py
+++ extra-addons/pycon/__manifest__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'security/pycon_security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
