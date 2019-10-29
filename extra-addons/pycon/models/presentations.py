e337993ecae9532589c7b2608134f12a9b9ccf94
Nadia AFAKROUCH <nafakrouch@bloopark.de>
2019-10-29 11:26:36 +0100

    [ADD] presentation, model, view and menu

---
+++ extra-addons/pycon/models/presentations.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class PyconPresentation(models.Model):
+	_name = "pycon.presentation"
+	_description="Pycon Presentations"
+
+	name = fields.Char(string='Name', required=True)
+	date = fields.Datetime()
+
